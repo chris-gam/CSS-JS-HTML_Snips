f4c9886de713be548470fb8398a4ee1c01e2e439
Christos <9966818+chris-gam@users.noreply.github.com>
2019-10-08 14:29:27 +0300

    Create magnific-popup.js

---
+++ magnific-popup.js
@@ -0,0 +1,13 @@
+//jQuery needed
+//https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js needed
+
+$(function() {
+    $('.popup-youtube, .popup-vimeo').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 150,
+        preloader: false,
+        fixedContentPos: false
+    });
+});
