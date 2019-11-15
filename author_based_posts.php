1f1966695b0ba31d1c2f205106a146fc58a689e0
Christos <9966818+chris-gam@users.noreply.github.com>
2019-11-15 14:53:34 +0200

    Create author_based_posts.php

---
+++ author_based_posts.php
@@ -0,0 +1,10 @@
+	<?php
+  global $post;
+  $the_author = get_the_author_meta( 'nickname' );
+  echo $the_author;
+  $args = array( 'posts_per_page' => 3, 'post_type'=> 'post', 'author_name' => $the_author );
+  $the_posts = get_posts( $args );
+  foreach( $the_posts as $post ) : setup_postdata($post); ?>
+    <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+  <?php endforeach; 
+  ?>
