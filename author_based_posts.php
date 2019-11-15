ed71f801e7ac6b17c25b95a7c18388771496ecb1
Christos <9966818+chris-gam@users.noreply.github.com>
2019-11-15 14:59:21 +0200

    Update author_based_posts.php

--- author_based_posts.php
+++ author_based_posts.php
@@ -1,7 +1,7 @@
 <?php
   global $post;
   $the_author = get_the_author_meta( 'nickname' );
-  echo $the_author;
+  //echo $the_author;
   $args = array( 'posts_per_page' => 3, 'post_type'=> 'post', 'author_name' => $the_author );
   $the_posts = get_posts( $args );
   foreach( $the_posts as $post ) : setup_postdata($post); ?>
